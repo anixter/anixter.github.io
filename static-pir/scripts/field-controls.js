69eeff4d760de17727bd4e02248e2e4cca1b7ef4
Matthew <mcoats@gmail.com>
2017-04-10 17:24:43 -0400

    Putting in new fields
    New fields, new field requirements, some of the JS stuff, and new
    layout.

---
+++ static-pir/scripts/field-controls.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  var date = new Date();
+  var today = new Date(date.getFullYear(), date.getMonth(), date.getDate());
+  var end = new Date(date.getFullYear(), date.getMonth(), date.getDate());
+
+  $('#date-requested').datepicker({
+format: "mm/dd/yyyy",
+startDate: today,
+  });
+
+  $('#date-requested').datepicker('setDate', today);
+  
+});
