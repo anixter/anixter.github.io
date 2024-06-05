f0e9009fa5122f090455c0647d832b6bb784c0f7
Matthew Coats <matthew.coats@anixter.com>
2024-06-05 16:31:45 -0400

    Changes to dashboards
    Changes to dashboards after meeting

--- epc/iba-dashboards/scripts/financials.js
+++ epc/iba-dashboards/scripts/financials.js
@@ -63,71 +63,12 @@ const releaseTotalsByYear = document.getElementById('releaseTotalsByYear');
  }
     });
 
-    const sentVsOrdered = document.getElementById('sentVsOrdered');
-
-    new Chart(sentVsOrdered, {
-      type: 'pie',
-      data: {
-        labels: ['Sent', 'Ordered'],
-        datasets: [{
-          label: '',
-          data: [11817961,11376957],
-          backgroundColor: [
-            '#087E8B',
-            '#FF5A5F'
-          ],
-          borderWidth: 1
-
-        },
-
-        
-        ]
-      },
-      options: {
-  plugins:{
-   legend: {
-    display: true
-   }
-  }
- }
-    });
-
-    const top5ProjectsSent = document.getElementById('top5ProjectsSent');
-
-    new Chart(top5ProjectsSent, {
-      type: 'doughnut',
-      data: {
-        labels: ['Ford Blue Oval', 'Magnolia', 'Tesla Orlando', 'Tesla Cranbury', 'Tesla Reno'],
-        datasets: [{
-          label: 'Amount Sent in Dollars',
-          data: [5245898,2471755,1085411,1259647,927190],
-          backgroundColor: [
-            '#EAC435',
-            '#345995',
-            '#03CEA4',
-            '#FB4D3D',
-            '#CA1551'
-          ],
-          borderWidth: 1
-
-        },
-
-        
-        ]
-      },
-      options: {
-  plugins:{
-   legend: {
-    display: true
-   }
-  }
- }
-    });
+   
 
         const top5Releases = document.getElementById('top5Releases');
 
     new Chart(top5Releases, {
-      type: 'doughnut',
+      type: 'bar',
       data: {
         labels: ['SUB Cranbury', 'SUB Reno', 'SUB Aslip', 'SUB Orlando', 'SUB Houston'],
         datasets: [{
@@ -188,69 +129,9 @@ const releaseTotalsByYear = document.getElementById('releaseTotalsByYear');
     });
 
 
-     const biggestMonths = document.getElementById('biggestMonths');
+     
 
-    new Chart(biggestMonths, {
-      type: 'doughnut',
-      data: {
-        labels: ['June', 'May', 'November', 'October', 'April'],
-        datasets: [{
-          label: 'Total Sent in Dollars',
-          data: [7956014,6262659,5463775,2878640,2156530],
-          backgroundColor: [
-            '#4059AD',
-            '#6B9AC4',
-            '#97D8C4',
-            '#D7EEE6',
-            '#F4B942'
-          ],
-          borderWidth: 1
-
-        },
-
-        
-        ]
-      },
-      options: {
-  plugins:{
-   legend: {
-    display: true
-   }
-  }
- }
-    });
-
-    const lowestMonths = document.getElementById('lowestMonths');
-
-    new Chart(lowestMonths, {
-      type: 'pie',
-      data: {
-        labels: ['December', 'February', 'September', 'July', 'January'],
-        datasets: [{
-          label: 'Total Sent in Dollars',
-          data: [1325826,1393492,1404598,1489707,1630944],
-          backgroundColor: [
-            '#EAC435',
-            '#345995',
-            '#03CEA4',
-            '#FB4D3D',
-            '#CA1551'
-          ],
-          borderWidth: 1
-
-        },
-
-        
-        ]
-      },
-      options: {
-  plugins:{
-   legend: {
-    display: true
-   }
-  }
- }
-    });
+    
 
 
      const top5CustomersSent = document.getElementById('top5CustomersSent');
